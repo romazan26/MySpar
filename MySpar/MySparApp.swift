e583e37198421cd5d39ce8541de57716f845e49a
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-02-01 22:11:10 +0300

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Роман on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
